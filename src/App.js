a81770ba198b823a9036beb29b607e65768f86e1
ahmed ali <aahmedabdi98@gmail.com>
2021-02-19 23:57:41 +0100

    small change

--- src/App.js
+++ src/App.js
@@ -26,11 +26,14 @@ function App() {
 								<div className="spotlight">
 									<div className="content">
 										<header className="major">
-											<h2>utvecklings områden</h2>
+											<h2>kort om mig</h2>
 										</header>
-										<p>Sed lorem ipsum dolor sit amet nullam consequat feugiat consequat magna
-										adipiscing magna etiam amet veroeros. Lorem ipsum dolor tempus sit cursus.
-										Tempus nisl et nullam lorem ipsum dolor sit amet aliquam.</p>
+										<p>
+										jag heter Ahmed Ali och är 22år gammal. Studerar Frontendutveckling, som person är jag lättlärd, villig
+										och redo att lära mig nya saker. Jag har stora ambitioner och är samtidigt målmedveten och målinriktad i
+										de fronter jag väljer att jobba/studera inom.
+
+										</p>
 										<ul className="actions">
 										</ul>
 									</div>
@@ -44,12 +47,6 @@ function App() {
 				{/* <!-- Footer --> */}
 					<footer id="footer">
 						<section>
-							<h2>Aliquam sed mauris</h2>
-							<p>Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat magna adipiscing tempus etiam dolore veroeros. eget dapibus mauris. Cras aliquet, nisl ut viverra sollicitudin, ligula erat egestas velit, vitae tincidunt odio.</p>
-							<ul className="actions">
-							</ul>
-						</section>
-						<section>
 							<h2>kontakt uppgifter</h2>
 							<dl className="alt">
 								<dt>Address</dt>
@@ -57,7 +54,7 @@ function App() {
 								<dt>Phone</dt>
 								<dd>073 674 31 49</dd>
 								<dt>Email</dt>
-								<dd><a href="#">Aahmedabdi98@gmail.com</a></dd>
+								<dd><a href="mailto:aahmedabdi98@gmail.com">Aahmedabdi98@gmail.com</a></dd>
 							</dl>
 						</section>
 					</footer>
